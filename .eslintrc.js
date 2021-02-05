a7559d39b5920ee375911988b6c09c609f246d17
Steven Kanberg <skanberg@kanbergs.com>
2021-02-05 14:40:00 -0800

    Updated linting rules to AirBnB; corrected new linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,46 @@
 module.exports = {
-    "extends": "eslint:recommended",
-    "rules": {
-        "semi": [2, "always"],
-        "indent": [2, 4],
-        "no-return-await": 0,
-        "space-before-function-paren": [2, {
-            "named": "never",
-            "anonymous": "never",
-            "asyncArrow": "always"
-        }],
-        "template-curly-spacing": [2, "always"]
-    }
+  extends: 'eslint-config-airbnb-base',
+  rules: {
+    semi: [2, 'always'],
+    indent: ['error', 2],
+    'no-return-await': 0,
+    'space-before-function-paren': [2, {
+      named: 'never',
+      anonymous: 'never',
+      asyncArrow: 'always',
+    }],
+    'template-curly-spacing': [2, 'always'],
+    'linebreak-style': 0,
+    'class-methods-use-this': 0,
+    'no-restricted-syntax': 'off',
+    'no-await-in-loop': 'off',
+    'max-len': [
+      'error',
+      {
+        code: 100,
+        tabWidth: 2,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+      },
+    ],
+    curly: [
+      'error',
+      'multi-or-nest',
+    ],
+    'no-plusplus': 'off',
+    'default-case': 'off',
+  },
+  env: {
+    es2020: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+      impliedStrict: true,
+    },
+  },
 };
--- webServer.js
+++ webServer.js
@@ -1,37 +1,41 @@
+/* eslint-disable no-multi-assign */
 const path = require('path');
 const { spawn } = require('cross-spawn');
+const logger = require('console').log;
 
 const ENV_FILE = path.join(__dirname, '.env');
 require('dotenv').config({ path: ENV_FILE });
 
 // Checks if NPM is installed on the system
+// eslint-disable-next-line no-undef
 exports.CheckNpm = CheckNpm = async () => {
-    const isNpmInstalled = spawn('npm', ['--version'], { stdio: 'inherit' });
-    await isNpmInstalled.on('error', (e) => {
-      console.log('\nWeb server unable to start...');
-      e.message = `Cannot execute command. Please verify 'npm' installation and try again.`;
-      console.log(`${e.stack}\n`);
-      isPythonInstalled.emit('CHECK_FAILED');
-      process.exit();
-    });
-    await isNpmInstalled.on('exit', () => {
-      console.log('\nNpm installation verified...');
-      isNpmInstalled.emit('CHECK_SUCCEEDED');
-    });
-    return isNpmInstalled;
-}
+  const isNpmInstalled = spawn('npm', ['--version'], { stdio: 'inherit' });
+  await isNpmInstalled.on('error', (e) => {
+    logger('\nWeb server unable to start...');
+    e.message = 'Cannot execute command. Please verify \'npm\' installation and try again.';
+    logger(`${ e.stack }\n`);
+    isNpmInstalled.emit('CHECK_FAILED');
+    process.exit();
+  });
+  await isNpmInstalled.on('exit', () => {
+    logger('\nNpm installation verified...');
+    isNpmInstalled.emit('CHECK_SUCCEEDED');
+  });
+  return isNpmInstalled;
+};
 
 // Calls 'npx serve' starting the web server
+// eslint-disable-next-line no-undef
 exports.WebServer = WebServer = async () => {
   setTimeout(() => {
-    console.log('Web server started...');
+    logger('Web server started...');
   }, 1000);
 
-  const startWebServer = spawn('npx serve', [`${process.env.WEB_SERVER_ROOT_DIRECTORY}`, '-l', `${process.env.WEB_SERVER_PORT}`], { stdio: 'inherit' });
+  const startWebServer = spawn('npx serve', [`${ process.env.WEB_SERVER_ROOT_DIRECTORY }`, '-l', `${ process.env.WEB_SERVER_PORT }`], { stdio: 'inherit' });
   await startWebServer.on('error', (e) => {
-    console.log('\nWeb server unable to start...');
-    e.message = `Cannot execute 'npx serve' command. Please verify installation/configuration and try again.`;
-    console.log(`${e.stack}\n`);
+    logger('\nWeb server unable to start...');
+    e.message = 'Cannot execute \'npx serve\' command. Please verify installation/configuration and try again.';
+    logger(`${ e.stack }\n`);
     process.exit();
   });
 };
